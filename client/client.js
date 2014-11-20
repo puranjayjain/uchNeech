39781b5ea0efbbc0c4bce83fdca82b77f7ce027d
Pranay <greatpranayshah@gmail.com>
2014-11-20 13:47:59 +0530

    Working, settings need to be changed, UI needs to improve

--- client/client.js
+++ client/client.js
@@ -1,10 +1,19 @@
 Meteor.startup (function ()
 	{
-		
+		Session.setDefault ('view', 'welcome');
 	});
+var first, flip;
 devices = new ReactiveVar (0);
 states = new ReactiveVar (0);
 selector = new ReactiveVar (0);
+
+Template.navbar.events (
+	{
+		'click a.pull-left' : function ()
+		{ 
+			Session.set ('view', 'welcome');
+		}
+	});
 Template.tab.events (
 	{
 		'click a' : function (event, template)
@@ -45,13 +54,13 @@ Template.table.helpers (
 		'alive' : function ()
 		{
 			var sel = selector.get();
-			sel = (_.extend (sel, {state: 'alive'})); console.log (sel);
+			sel = (_.extend (sel, {state: 'alive'}));
 			return Hosts.find (sel);
 		},
 		'dead' : function ()
 		{
 			var sel = selector.get();
-			sel = (_.extend (sel, {state: 'dead'}));console.log (sel);
+			sel = (_.extend (sel, {state: 'dead'}));
 			return Hosts.find (sel);
 		}
 	});
@@ -60,9 +69,44 @@ Template.cell.helpers (
 		'ip': function () { return this.ip;},
 		'since' : function ()
 		{
-			var date = new Date();
-			var one_min=1000*60*60;
-			return (this['state']==='alive') ? (date - _.last (this['alive']))/one_min : (date - _.last (this['dead']))/one_min;
+			var date = new Date().getTime();
+			var one_min=1000*60;
+			var xyz = (this['state']==='alive') ? (date - _.last (this['alive']).getTime()) : (date - _.last (this['dead']).getTime());
+			// console.log (xyz+ " - "+ this['ip']);
+			return Math.round (xyz/one_min);
+		}
+	});
+
+Template.cell.events (
+	{
+		'click li' : function (event, template )
+		{
+			Session.set ('view', 'history');
+			Session.set ('selectedHost', this);
+		}
+	});
+Template.history.helpers (
+	{
+		'ip' : function ()
+		{
+			return Session.get ('selectedHost')['ip'];
+		},
+		'hist' : function ()
+		{ 
+			var host = Session.get ('selectedHost');
+			hist = _.sortBy(_.union (host['alive'], host['dead']), function (num) {return num;});
+			return hist;
+		},
+		which : function ()
+		{
+			var host = Session.get ('selectedHost'); 
+			if (first === undefined)
+			{  first = (_.first (host['alive']) < _.first (host ['dead']))? 'up': 'down';}
+			if (first ==='up') {flip = 'fa-arrow-up'; first = null;}
+			else if (first === 'down') {flip = 'fa-arrow-down'; first = null;}
+			else if  (flip === 'fa-arrow-down') {flip = 'fa-arrow-up';}
+			else if (flip === 'fa-arrow-up') {flip = 'fa-arrow-down';}
+			return flip;
 		}
 	});
 Template.body.helpers (
@@ -72,3 +116,8 @@ Template.body.helpers (
 			return !(devices.get()=== 0 || states.get()===0);
 		}
 	});
+
+Template.registerHelper ('state', function (status)
+			{ 
+				return (Session.equals ('view', status));
+			});
